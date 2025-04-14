e4b79d4f8a63a837c5b10e928aac26893717f687
Yohannes Luminari <67595372+yohannesluminari@users.noreply.github.com>
2025-04-14 16:41:02 +0200

    MODALE DELETE

--- MyBatisJpa/src/app/pages/home/home.component.ts
+++ MyBatisJpa/src/app/pages/home/home.component.ts
@@ -10,7 +10,10 @@ import { EmployeeService } from '../../service/employee.service';
 })
 export class HomeComponent implements OnInit {
   employees: EmployeeResponse[] = [];
-  isFormVisible: boolean = false;  // Variabile per controllare la visibilità del form
+  isFormVisible: boolean = false;
+
+  confirmModalVisible: boolean = false;
+  deleteCandidateId: number | null = null;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -18,7 +21,6 @@ export class HomeComponent implements OnInit {
     this.loadEmployees();
   }
 
-  // Carica la lista dei dipendenti
   loadEmployees(): void {
     this.employeeService.getAll().subscribe({
       next: (data) => {
@@ -30,29 +32,38 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  deleteEmployee(id: number): void {
-    if (confirm('Sei sicuro di voler eliminare questo dipendente?')) {
-      this.employeeService.delete(id).subscribe({
-        next: (msg) => {
-          console.log(msg);
-          this.loadEmployees(); // Ricarica la lista dopo l'eliminazione
+  showDeleteModal(id: number): void {
+    this.deleteCandidateId = id;
+    this.confirmModalVisible = true;
+  }
+
+  cancelDelete(): void {
+    this.confirmModalVisible = false;
+    this.deleteCandidateId = null;
+  }
+
+  confirmDelete(): void {
+    if (this.deleteCandidateId !== null) {
+      this.employeeService.delete(this.deleteCandidateId).subscribe({
+        next: () => {
+          this.loadEmployees();
+          this.confirmModalVisible = false;
+          this.deleteCandidateId = null;
         },
         error: (err) => {
           console.error('Errore durante l\'eliminazione', err);
+          this.confirmModalVisible = false;
         }
       });
     }
   }
 
-
-  // Mostra o nasconde il form
   toggleForm(): void {
     this.isFormVisible = !this.isFormVisible;
   }
 
-  // Gestisce l'evento quando un dipendente è stato salvato
   onEmployeeSaved(): void {
-    this.isFormVisible = false;  // Nasconde il form
-    this.loadEmployees();        // Ricarica la lista dei dipendenti
+    this.isFormVisible = false;
+    this.loadEmployees();
   }
 }
